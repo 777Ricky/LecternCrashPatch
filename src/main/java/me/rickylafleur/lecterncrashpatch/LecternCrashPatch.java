1a588aba0023ba157a05f8130f88a82349494430
777Ricky <zach.varnick@gmail.com>
2022-04-05 20:44:41 -0500

    init

---
+++ src/main/java/me/rickylafleur/lecterncrashpatch/LecternCrashPatch.java
@@ -0,0 +1,26 @@
+package me.rickylafleur.lecterncrashpatch;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.net.http.WebSocket;
+
+public final class LecternCrashPatch extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onLecternCrashAttempt(InventoryClickEvent e) {
+        if (e.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY && e.getCursor().getType() == Material.AIR && e.getView().getTitle().equals("Lectern") && e.getClick().toString().equals("SHIFT_LEFT") && e.getHotbarButton() == -1) {
+            e.setCancelled(true);
+        }
+    }
+}
