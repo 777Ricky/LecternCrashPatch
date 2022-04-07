8755306e51c376b74a268e0e5c5e4700c2651592
777Ricky <zach.varnick@gmail.com>
2022-04-06 22:20:18 -0500

    change fix method

--- src/main/java/me/rickylafleur/lecterncrashpatch/LecternCrashPatch.java
+++ src/main/java/me/rickylafleur/lecterncrashpatch/LecternCrashPatch.java
@@ -1,11 +1,10 @@
 package me.rickylafleur.lecterncrashpatch;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LecternCrashPatch extends JavaPlugin implements Listener {
@@ -17,7 +16,7 @@ public final class LecternCrashPatch extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onLecternCrashAttempt(InventoryClickEvent e) {
-        if (e.getAction() == InventoryAction.MOVE_TO_OTHER_INVENTORY && e.getCursor().getType() == Material.AIR && e.getView().getTitle().equals("Lectern") && e.getClick().toString().equals("SHIFT_LEFT") && e.getHotbarButton() == -1) {
+        if (e.getInventory().getType() == InventoryType.LECTERN) {
             e.setCancelled(true);
         }
     }
